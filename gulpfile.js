0578fcaa64995c6cca79177bb53f6b70f4a31b78
Rafis-dev <71827687+Rafis-dev@users.noreply.github.com>
2022-04-30 12:32:28 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,42 @@ const { src, dest, watch, parallel, series } = require('gulp');
 
 ///// ПЛАГИНЫ /////
 //Для конвертации препроцессора SASS
-const sass         = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 // Для конкатинации (соединения) файлов
-const concat       = require('gulp-concat');
+const concat = require('gulp-concat');
 // Для автоматического обновления браузера
-const browserSync  = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 // Для минификации js файлов
-const uglify       = require('gulp-uglify-es').default;
+const uglify = require('gulp-uglify-es').default;
 // Для подстановки вендорных префиксов
 const autoprefixer = require('gulp-autoprefixer');
 // Для сжатия картинок
-const imagemin     = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 // Чтобы не сжимать уже ранее сжатые изображения
-const newer        = require('gulp-newer');
+const newer = require('gulp-newer');
 // Для конвертации изображений в формат webp
-const webp         = require('gulp-webp');
+const webp = require('gulp-webp');
 // Для автоматичского оборачивания изображений в тег picture
-const webphtml     = require('gulp-webp-html');
+const webphtml = require('gulp-webp-html');
 // Для автоматичского оборачивания изображений в тег picture
-const webpcss      = require('gulp-webp-css-fixed');
+const webpcss = require('gulp-webp-css-fixed');
 // Для очистки папок
-const del          = require('del');
+const del = require('del');
 // Для работы с html-шаблонами
-const fileinclude  = require('gulp-file-include');
+const fileinclude = require('gulp-file-include');
 // Для конвертации шрифтов
-const ttf2woff     = require('gulp-ttf2woff');
-const ttf2woff2    = require('gulp-ttf2woff2');
-const fonter       = require('gulp-fonter');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const fonter = require('gulp-fonter');
 // Для группировки media запросов в определенном порядке
-const groupcss     = require('gulp-group-css-media-queries')
+const groupcss = require('gulp-group-css-media-queries')
 
 
 
 ///// АВТОМАТИЧЕСКОЕ ОБНОВЛЕНИЕ БРАУЗЕРА /////
 function browsersync() {
   browserSync.init({
-    server : {
+    server: {
       baseDir: 'dist/'
     }
   });
@@ -51,30 +51,29 @@ function cleanDist() {
 //// HTML ШАБЛОНЫ /////
 function html() {
   return src('app/*.html')
-  .pipe(fileinclude())
-  .pipe(webphtml())
-  .pipe(dest('dist'))
-  .pipe(browserSync.stream())
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream())
 }
 
 //// Конвертация шрифтов в woff и woff2 /////
 // этот таск вызываем отдельно в самом начале для конвертации шрифтов в ttf формат  
-function tottf() {                     
-  return src('app/fonts/**/*')     
-  .pipe(fonter({
-    formats: ["ttf"]
-  }))
-  .pipe(dest('app/fonts'))
+function tottf() {
+  return src('app/fonts/**/*')
+    .pipe(fonter({
+      formats: ["ttf"]
+    }))
+    .pipe(dest('app/fonts'))
 }
 
 function fonts() {
   return src('app/fonts/**/*.ttf')
-  .pipe(ttf2woff2())
-  .pipe(src('app/fonts/**/*.ttf'))
-  .pipe(ttf2woff())
-  .pipe(newer('dist/fonts'))
-  .pipe(dest('dist/fonts'))
-  .pipe(browserSync.stream())
+    .pipe(ttf2woff2())
+    .pipe(src('app/fonts/**/*.ttf'))
+    .pipe(ttf2woff())
+    .pipe(newer('dist/fonts'))
+    .pipe(dest('dist/fonts'))
 }
 
 
@@ -84,26 +83,26 @@ function fonts() {
 ///// СЖАТИЕ КАРТИНОК /////
 function images() {
   return src('app/images/**/*')
-  .pipe(newer('dist/images'))
-  .pipe(webp())
-  .pipe(dest('dist/images'))
-  .pipe(src('app/images/**/*'))
-  .pipe(newer('dist/images'))
-  .pipe(imagemin(
-    [
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ]
-  ))
-  .pipe(dest('dist/images'))
-  .pipe(browserSync.stream())
+    .pipe(newer('dist/images'))
+    .pipe(webp())
+    .pipe(dest('dist/images'))
+    .pipe(src('app/images/**/*'))
+    .pipe(newer('dist/images'))
+    .pipe(imagemin(
+      [
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: true },
+            { cleanupIDs: false }
+          ]
+        })
+      ]
+    ))
+    .pipe(dest('dist/images'))
+    .pipe(browserSync.stream())
 }
 ///// ОБРАБОТКА JS /////
 function scripts() {
@@ -111,38 +110,38 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js', //*JQUERY. При ненадобности библиотеки можно строчку закоментировать*
     'app/js/main.js'
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(dest('dist/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream())
 }
 
 
 ///// ОБРАБОТКА СТИЛЕЙ /////
 // SASS файлы преобразовываем в css файлы двух типов: расширенный (expanded) и сжатый (compressed).Непосредственно к проекту для оптимизации подключается сжатый вариант
 function styles() {
-  return src(['app/sass/style.sass', 'node_modules/normalize.css/normalize.css'], { sourcemaps: true })
-  .pipe(sass({outputStyle: 'expanded'}))
-  .pipe(concat('style.css'))
-  .pipe(webpcss())
-  .pipe(autoprefixer({
-    grid: true
-  }))
-  .pipe(groupcss())
-  .pipe(dest('app/css', { sourcemaps: true }))
-  .pipe(dest('dist/css'))
-  .pipe(src(['app/sass/style.sass', 'node_modules/normalize.css/normalize.css'], { sourcemaps: true }))
-  .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(concat('style.min.css'))
-  .pipe(webpcss())
-  .pipe(autoprefixer({
-    grid: true
-  }))
-  .pipe(groupcss())
-  .pipe(dest('app/css', { sourcemaps: true }))
-  .pipe(dest('dist/css'))
-  .pipe(browserSync.stream())
+  return src(['node_modules/normalize.css/normalize.css', 'app/sass/style.sass'], { sourcemaps: true })
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(concat('style.css'))
+    .pipe(webpcss())
+    .pipe(autoprefixer({
+      grid: true
+    }))
+    .pipe(groupcss())
+    .pipe(dest('app/css', { sourcemaps: true }))
+    .pipe(dest('dist/css'))
+    .pipe(src(['node_modules/normalize.css/normalize.css', 'app/sass/style.sass'], { sourcemaps: true }))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(webpcss())
+    .pipe(autoprefixer({
+      grid: true
+    }))
+    .pipe(groupcss())
+    .pipe(dest('app/css', { sourcemaps: true }))
+    .pipe(dest('dist/css'))
+    .pipe(browserSync.stream())
 }
 
 
@@ -153,7 +152,7 @@ function build() {
     'app/css/style.css',
     'app/js/main.min.js',
     'app/js/main.js',
-  ], {base: 'app'})
+  ], { base: 'app' })
     .pipe(dest('dist'))
 }
 //// СЛЕЖЕНИЕ ЗА ИЗМЕНЕНИЯМИ /////
