e6a32e270bfddc4c0867f8aa8d9664869fa29743
Rafis-dev <71827687+Rafis-dev@users.noreply.github.com>
2022-06-16 16:29:44 +0300

    Add files via upload

--- app/js/files/_burger.js
+++ app/js/files/_burger.js
@@ -2,6 +2,7 @@ const iconMenu = document.querySelector('.icon-menu');
 if (iconMenu) {
   const menuBody = document.querySelector('.menu__body');
   iconMenu.addEventListener("click", function (e) {
-    iconMenu.classList.toggle('_active')
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
   });
 }
