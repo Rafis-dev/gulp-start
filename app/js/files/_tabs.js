018973ff89071ed27f2d554b00645977be7cf734
Rafis-dev <71827687+Rafis-dev@users.noreply.github.com>
2022-08-04 12:48:37 +0300

    Add files via upload

---
+++ app/js/files/_tabs.js
@@ -0,0 +1,9 @@
+// JQUERY
+$('.tab').on('click', function (e) {
+  e.preventDefault();
+  $('.tab').removeClass('tab--active');
+  $('.tabs-content').removeClass('tabs-content--active');
+
+  $(this).addClass('tab--active');
+  $($(this).attr('href')).addClass('tabs-content--active');
+});
