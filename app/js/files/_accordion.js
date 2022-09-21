da2a5abaaa211bee8d95cb42dc9ecab1c7caa8bf
Rafis-dev <71827687+Rafis-dev@users.noreply.github.com>
2022-09-21 14:18:40 +0300

    Add files via upload

--- app/js/files/_accordion.js
+++ app/js/files/_accordion.js
@@ -1,6 +1,12 @@
 // Аккордеон
 document.addEventListener('DOMContentLoaded', () => {
   const accordions = document.querySelectorAll('.accordion')
+  const accordion = document.querySelector('.accordion');
+  const content = accordion.querySelector('.accordion__content');
+  // Чтобы одна колонка по умолчанию была открыта
+  if (accordion.classList.contains('open')) {
+    content.style.maxHeight = content.scrollHeight + 'px';
+  }
 
   accordions.forEach(el => {
     el.addEventListener('click', (e) => {
@@ -8,8 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const control = self.querySelector('.accordion__control');
       const content = self.querySelector('.accordion__content');
       self.classList.toggle('open');
-
-      // аттрибудты для скрин ридеров
+      // Ниже код для скрин-ридеров
       if (self.classList.contains('open')) {
         control.setAttribute('aria-expanded', true);
         content.setAttribute('aria-hidden', false);
