6993e29e525665b6f238563c45875170100126e7
Rafis-dev <71827687+Rafis-dev@users.noreply.github.com>
2022-10-06 15:45:42 +0300

    Add files via upload

---
+++ app/chunks/_tabs.js
@@ -0,0 +1,33 @@
+// JQUERY
+$('.tab').on('click', function (e) {
+  e.preventDefault();
+  $($(this).siblings()).removeClass('tab--active');
+  $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+  $(this).addClass('tab--active');
+  $($(this).attr('href')).addClass('tabs-content--active');
+});
+
+// Вебкадеми с изменениями
+
+// Нашли все заголовки табов по data атрибуту
+const tabHeaders = document.querySelectorAll('[data-tab]');
+// Нашли все контент боксы
+const contentBoxes = document.querySelectorAll('[data-tab-content]');
+
+tabHeaders.forEach(function (item) {
+  item.addEventListener('click', function () {
+    // Добавляем и убираем active у заголовков табов
+    document.querySelectorAll('.tabs-list__item').forEach((block) => block.classList.remove('active'));
+    this.classList.add('active');
+    // 1. Скрыть все content box
+    contentBoxes.forEach(function (item) {
+      item.classList.remove('active');
+    });
+
+    // 2. Выбрать нужный content box и показать его
+    const contentBox = document.querySelector('#' + this.dataset.tab);
+    contentBox.classList.add('active');
+
+  })
+})
